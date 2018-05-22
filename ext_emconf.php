0d40be26bbe72ac3db531ca40a802169f1300b66
Simon Schmidt <sfs@marketing-factory.de>
2018-05-22 16:24:01 +0200

    update ext_emconf.php
    add dependancy for fsc

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,6 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.2-7.99.99',
+            'fluid_styled_content' => '8.7.13- 8.7.99',
             'typo3' => '8.7.10-8.99.99'
         ]
     ],
